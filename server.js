bbac946eb277ebcf9737b02bf0a5b06e619fa588
philomather <philomather@gmail.com>
2018-04-03 01:41:36 +0200

    Changed the static file directory

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express'),
     
 Object.assign=require('object-assign')
 
-app.use(express.static('public'));
+app.use(express.static('/views'));
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
