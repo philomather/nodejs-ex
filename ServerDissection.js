ae13584db735d57e86c3be8bb1054896500b9a5c
Pat Leacy <leacy.pat@gmail.com>
2018-05-31 17:49:25 +0200

    return thegnaw

--- ServerDissection.js
+++ ServerDissection.js
@@ -1,13 +1,28 @@
+// Server.js dissection
+
 //  OpenShift sample Node application
 var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
     
+    // var creates variables and assigns to those variables modules
+    // Expressjs is a web framework for node
+
+
+
 Object.assign=require('object-assign')
+
+
 
 app.use(express.static('views'));
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
+
+
+
+
+
+
 
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
